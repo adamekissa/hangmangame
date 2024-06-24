26c2176ae58027de072bb3366081bd3d1897955a
Mohammed Adam <adamekissa@gmail.com>
2024-06-24 10:25:58 +0100

    interface layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Button from './components/Button'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +9,35 @@ function App() {
     <>
       <div>
         <h3>Welcome to the hangman game</h3>
+
+        <div className='button--div'>
+          <Button text="a" />
+          <Button text="b" />
+          <Button text="c" />
+          <Button text="d" />
+          <Button text="e" />
+          <Button text="f" />
+          <Button text="g" />
+          <Button text="h" />
+          <Button text="i" />
+          <Button text="j" />
+          <Button text="k" />
+          <Button text="l" />
+          <Button text="m" />
+          <Button text="n" />
+          <Button text="o" />
+          <Button text="p" />
+          <Button text="q" />
+          <Button text="r" />
+          <Button text="s" />
+          <Button text="t" />
+          <Button text="u" />
+          <Button text="v" />
+          <Button text="w" />
+          <Button text="x" />
+          <Button text="y" />
+          <Button text="z" />
+        </div>
       </div>
     </>
   )
---
+++ src/components/Button.jsx
@@ -0,0 +1,5 @@
+export default function Button(props) {
+    return (
+        <button className="alphabet--button" >{props.text}</button>
+    )
+}
